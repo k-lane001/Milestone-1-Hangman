8e7be05676a26ede8ab5eba80a565f9f3c44d41b
Kaylie Lane <k.lane5612@gmail.com>
2022-05-01 17:53:48 -0400

    append img to html fm js file using addElement at onload, corrected image paths to assets folder

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
-//Image Array
 
-const gallowsImg = new Array();
+document.body.onload = addElement;
 
-gallowsImg[0] = new Image();
-gallowsImg[0].src ='.assets/images/Start.jpeg';
+function addElement() {
+    const img = document.createElement('img');
+    img.src = './assets/images/Start.jpeg';
+    document.getElementById('gallows').appendChild(img);
+}
 
-gallowsImg[1] = new Image();
-gallowsImg[1].src ='.assets/images/gallows2.jpeg';
+// Image Array
+var imgArray = new Array();
 
-gallowsImg[2] = new Image();
-gallowsImg[2].src ='.assets/images/gallows3.jpeg';
+imgArray[0] = new Image();
+imgArray[0].src = './assets/images/Start.jpeg';
 
-gallowsImg[3] = new Image();
-gallowsImg[3].src ='.assets/images/gallows4.jpeg';
+imgArray[1] = new Image();
+imgArray[1].src = './assets/images/gallows2.jpg';
 
-gallowsImg[4] = new Image();
-gallowsImg[4].src ='.assets/images/gallows5.jpeg';
+imgArray[2] = new Image();
+imgArray[2].src =  './assets/images/gallows3.jpg';
 
-gallowsImg[5] = new Image();
-gallowsImg[5].src ='.assets/images/gallows6.jpeg';
+imgArray[3] = new Image();
+imgArray[3].src = './assets/images/gallows4.jpg';
 
-gallowsImg[6] = new Image();
-gallowsImg[6].src ='.assets/images/gallows7.jpeg';
+imgArray[4] = new Image();
+imgArray[4].src = './assets/images/gallows5.jpg';
 
-gallowsImg[7] = new Image();
-gallowsImg[7].src ='.assets/images/gallows8.jpeg';
+imgArray[5] = new Image();
+imgArray[5].src = './assets/images/gallows6.jpg';
 
-gallowsImg[8] = new Image();
-gallowsImg[8].src ='.assets/images/gallows9.jpeg';
+imgArray[6] = new Image();
+imgArray[6].src = './assets/images/gallows7.jpg';
 
-gallowsImg[9] = new Image();
-gallowsImg[9].src ='.assets/images/gallows10.jpeg';
+imgArray[7] = new Image();
+imgArray[7].src = './assets/images/gallows8.jpg';
 
-gallowsImg[10] = new Image();
-gallowsImg[10].src ='.assets/images/End.jpeg';
+imgArray[8] = new Image();
+imgArray[8].src = './assets/images/gallows9.jpg';
+
+imgArray[9] = new Image();
+imgArray[9].src = './assets/images/gallows10.jpg';
+
+imgArray[10] = new Image();
+imgArray[10].src = './assets/images/End.png';
 
 //Names Array
 const names = ['freddy', 'jason', 'michael', 'pinhead', 'pennywise', 'chucky', 'norman', 'hannibal', 'jack', 'ghostface', 'frankenstein', 'dracula', 'mummy', ];
 
 //Image Loop Function
 function nextImage() {
-    var img = document.getElementById('gallows');
-    for(var i=0; i<gallowsImg.length; i++) {
-        if gallowsImg[i].src == img.src) {
-            if(i === gallowsImg.length) {
-                document.getElementById('gallows').src = gallowsImg[0].src;
-                break;
-            }
-            document.getElementById('gallows').src = gallowsImg[+1].src;
-            break;
+    for(var i=0; i<imgArray.length; i++) {
+        imgArray[i];
         }
-    }
 }
+
 
